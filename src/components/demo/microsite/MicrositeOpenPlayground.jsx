e0ecfec81b0f971b37a0e01cc1300cf02e181ffa
juribemonsalve <uribemonsalvejuan@gmail.com>
2025-03-18 18:01:20 -0500

    feat: New fields functionality with react-movable, including bug fixes

--- src/components/demo/microsite/MicrositeOpenPlayground.jsx
+++ src/components/demo/microsite/MicrositeOpenPlayground.jsx
@@ -5,71 +5,24 @@ import { List, arrayMove } from "react-movable";
 import { Pencil, Trash, Plus, Palette, ImagePlus } from "lucide-react";
 import { ImageToBase64 } from '@/components/PopupLogoMicrositeOpen';
 import { ContrastText } from '@/components/AutomaticTextContrast';
-import Image from 'next/image'
-import footerSvg from '@/images/logos/footer.svg'
+import Image from 'next/image';
+import footerSvg from '@/images/logos/footer.svg';
 import generatePDF, { Margin } from 'react-to-pdf';
 
 export function MicrositeOpenPlayground() {
   const [fields, setFields] = React.useState([
-    {
-      id: "reference",
-      label: "Referencia",
-      subLabel: "(Referencia)",
-      type: "text",
-      required: true,
-
-    },
-    {
-      id: "payment_description",
-      label: "Descripción del pago",
-      subLabel: "(Descripción del pago)",
-      type: "text",
-      required: true,
-    },
-    {
-      id: "currency",
-      label: "Moneda",
-      subLabel: "(Moneda)",
-      type: "select",
-      options: ["Selecciona una opción", "Peso colombiano", "Dólar estadounidense"],
-      required: true,
-    },
-    {
-      id: "amount",
-      label: "Monto",
-      subLabel: "(Monto)",
-      type: "text",
-      required: true,
-    },
-    {
-      id: "buyer_id_type",
-      label: "Tipo de documento",
-      type: "select",
-      options: ["Selecciona una opción", "Cédula de ciudadanía", "Cédula de extranjería", "NIT"],
-    },
-    {
-      id: "buyer_id",
-      label: "Documento del comprador",
-      type: "text",
-    },
-    {
-      id: "buyer_name",
-      label: "Nombre del comprador",
-      type: "text",
-    },
-    {
-      id: "buyer_surname",
-      label: "Apellido del comprador",
-      type: "text",
-    },
-    {
-      id: "buyer_email",
-      label: "Correo electrónico",
-      type: "email",
-    },
+    { id: "reference", label: "Referencia", subLabel: "(Referencia)", type: "text", required: true },
+    { id: "payment_description", label: "Descripción del pago", subLabel: "(Descripción del pago)", type: "text", required: true },
+    { id: "currency", label: "Moneda", subLabel: "(Moneda)", type: "select", options: ["Selecciona una opción", "Peso colombiano", "Dólar estadounidense"], required: true },
+    { id: "amount", label: "Monto", subLabel: "(Monto)", type: "text", required: true },
+    { id: "buyer_id_type", label: "Tipo de documento", type: "select", options: ["Selecciona una opción", "Cédula de ciudadanía", "Cédula de extranjería", "NIT"] },
+    { id: "buyer_id", label: "Documento del comprador", type: "text" },
+    { id: "buyer_name", label: "Nombre del comprador", type: "text" },
+    { id: "buyer_surname", label: "Apellido del comprador", type: "text" },
+    { id: "buyer_email", label: "Correo electrónico", type: "email" },
   ]);
 
-  const [fieldCounter, setFieldCounter] = React.useState(6);
+  const [fieldCounter, setFieldCounter] = React.useState(9); // Starting from 9 since initial items go up to 8
   const protectedFields = ["reference", "payment_description", "currency", "amount"];
 
   const handleEdit = (id) => {
@@ -83,27 +36,19 @@ export function MicrositeOpenPlayground() {
     setFields(fields.filter(field => field.id !== id));
   };
 
+  const addNewRow = (numFields) => {
+    const newFields = Array(numFields).fill(null).map((_, index) => ({
+      id: `field_${fieldCounter + index}`,
+      label: `Campo ${fieldCounter + index}`,
+      type: "text",
+    }));
+    setFieldCounter(fieldCounter + numFields);
+    setFields([...fields, { id: `row_${fieldCounter}`, columns: newFields }]);
+  };
+
   const handleFieldSelection = (e) => {
     const numFields = parseInt(e.target.value);
     if (!isNaN(numFields)) addNewRow(numFields);
-  };
-
-  const addNewRow = (numFields) => {
-    setFieldCounter(prevCounter => {
-      const newFields = Array.from({ length: numFields }, () => {
-        const newCounter = prevCounter + 1;
-        return {
-          id: `field${newCounter}`,
-          label: `Nuevo campo ${newCounter}`,
-          type: "text",
-          required: false,
-        };
-      });
-
-      setFields(prevFields => [...prevFields, ...newFields]);
-
-      return prevCounter + numFields;
-    });
   };
 
   const targetRef = useRef();
@@ -114,25 +59,17 @@ export function MicrositeOpenPlayground() {
   const [colorBtn, setColorBtn] = useState("#64748B");
   const [colorSidePanel, setColorSidePanel] = useState("#F1F5F9");
 
-  // Enseñar el botón de cambio de color para el panel lateral
-
   const handleClickSidePanel = () => {
     setIsVisibleSidePanel(!isVisibleSidePanel);
   };
-
-  // Enseñar el botón de cambio de color para el botón
 
   const handleClickBtn = () => {
     setIsVisibleBtn(!isVisibleBtn);
   };
 
-  // Enseñar el botón de cambio de logo
-
   const handleClickLogo = () => {
     setIsVisibleLogo(!isVisibleLogo);
   };
-
-  // Esconder botones al descargar PDF
 
   const getContentSize = () => {
     const content = targetRef.current;
@@ -166,9 +103,6 @@ export function MicrositeOpenPlayground() {
     }, 1000);
   };
 
-  const [items, setItems] = React.useState(
-    Array.from(Array(5).keys()).map((val) => `Item ${val}`),
-  );
 
   return (
     <div>
@@ -177,13 +111,17 @@ export function MicrositeOpenPlayground() {
         <div className="flex flex-row gap-4 items-start cursor-pointer mt-4">
           <select
             id="fieldSelector"
-            className="p-2 bg-gray-500 text-white rounded-lg flex items-center justify-center cursor-pointer focus:outline-none hover:bg-gray-600">
+            className="p-2 bg-gray-500 text-white rounded-lg flex items-center justify-center cursor-pointer focus:outline-none hover:bg-gray-600"
+            onChange={handleFieldSelection}
+          >
             <option value="">Agregar Campo</option>
             <option value="1">1 Fila de 1 Campo</option>
             <option value="2">1 Fila de 2 Campos</option>
             <option value="3">1 Fila de 3 Campos</option>
           </select>
         </div>
+
+
       </div>
       <div ref={targetRef} className="mt-8 rounded-xl border border-gray-300 dark:border-gray-700 w-full">
         <div className="mx-auto w-full grow flex">
@@ -203,7 +141,7 @@ export function MicrositeOpenPlayground() {
                   <div className="md:mt-6 mb-24 md:mb-12 md:mx-auto flex flex-row w-full lg:w-4/5">
                     <form noValidate="" className="p-0 lg:p-0 md:p-2 flex flex-col">
                       <div className="form-layout w-full">
-                        
+
                         <List
                           values={fields}
                           onChange={({ oldIndex, newIndex }) =>
@@ -215,48 +153,97 @@ export function MicrositeOpenPlayground() {
                             </div>
                           )}
                           renderItem={({ value, props, isDragged, isSelected }) => {
-                            const isFullWidth = value.id === "reference" || value.id === "payment_description" || value.id === "buyer_email";
-                            return (
-                              <div
-                                {...props}
-                                key={props.key}
-                                style={{
-                                  ...props.style,
-                                  padding: '0.3rem',
-                                  cursor: isDragged ? 'grabbing' : 'grab',
-                                  borderRadius: '0.5rem',
-                                  backgroundColor: isDragged || isSelected ? '#EEE' : '#FFF',
-                                  zIndex: isDragged ? 1000 : 'auto'
-                                }}
-                                className={`flex flex-col ${isFullWidth ? 'col-span-2' : 'col-span-1'}`}>
-                                <div className="flex items-center gap-2">
-                                  <label className="text-sm font-medium text-gray-700 mb-2">{value.label}</label>
-                                  <span className="text-sm text-gray-500 mb-2">{value.subLabel}</span>
-                                  {protectedFields.includes(value.id) && (
-                                    <p className="text-red-500">*</p>
-                                  )}
+                            if (value.columns) {
+                              return (
+                                <div
+                                  {...props}
+                                  key={props.key}
+                                  style={{
+                                    ...props.style,
+                                    padding: '0.3rem',
+                                    cursor: isDragged ? 'grabbing' : 'grab',
+                                    borderRadius: '0.5rem',
+                                    backgroundColor: isDragged || isSelected ? '#EEE' : '#FFF',
+                                    zIndex: isDragged ? 1000 : 'auto',
+                                    display: 'grid',
+                                    gridTemplateColumns: `repeat(${value.columns.length}, 1fr)`,
+                                    gap: '8px',
+                                    width: '100%'
+                                  }}
+                                  className="col-span-2"
+                                >
+                                  {value.columns.map((field) => (
+                                    <div key={field.id} className="flex flex-col">
+                                      <div className="flex items-center gap-2">
+                                        <label className="text-sm font-medium text-gray-700 mb-2">{field.label}</label>
+                                      </div>
+                                      <div className="flex items-center gap-2">
+                                        <input
+                                          type="text"
+                                          className="w-full border border-gray-400 py-2 text-gray-900 outline-none placeholder:text-gray-400 focus:border-primary-300 rounded-lg"
+                                          id={field.id}
+                                        />
+                                        {isVisiblePDF && (
+                                          <button type="button" onClick={() => handleEdit(field.id)} className="text-blue-500">
+                                            <Pencil size={16} />
+                                          </button>
+                                        )}
+                                        {isVisiblePDF && !protectedFields.includes(field.id) && (
+                                          <button type="button" onClick={() => handleDelete(field.id)} className="text-red-500">
+                                            <Trash size={16} />
+                                          </button>
+                                        )}
+                                      </div>
+                                    </div>
+                                  ))}
                                 </div>
-                                <div className="flex items-center gap-2">
-                                  <input
-                                    type="text"
-                                    className="w-full border border-gray-400 py-2 text-gray-900 outline-none placeholder:text-gray-400 focus:border-primary-300 rounded-lg"
-                                    id={value.id}
-                                  />
-                                  {isVisiblePDF && (
-                                    <button type="button" onClick={() => handleEdit(value.id)} className="text-blue-500">
-                                      <Pencil size={16} />
-                                    </button>
-                                  )}
-                                  {isVisiblePDF && !protectedFields.includes(value.id) && (
-                                    <button type="button" onClick={() => handleDelete(value.id)} className="text-red-500">
-                                      <Trash size={16} />
-                                    </button>
-                                  )}
+                              );
+                            } else {
+                              const isFullWidth = value.id === "reference" || value.id === "payment_description" || value.id === "buyer_email";
+                              return (
+                                <div
+                                  {...props}
+                                  key={props.key}
+                                  style={{
+                                    ...props.style,
+                                    padding: '0.3rem',
+                                    cursor: isDragged ? 'grabbing' : 'grab',
+                                    borderRadius: '0.5rem',
+                                    backgroundColor: isDragged || isSelected ? '#EEE' : '#FFF',
+                                    zIndex: isDragged ? 1000 : 'auto'
+                                  }}
+                                  className={`flex flex-col ${isFullWidth ? 'col-span-2' : 'col-span-1'}`}
+                                >
+                                  <div className="flex items-center gap-2">
+                                    <label className="text-sm font-medium text-gray-700 mb-2">{value.label}</label>
+                                    <span className="text-sm text-gray-500 mb-2">{value.subLabel}</span>
+                                    {protectedFields.includes(value.id) && (
+                                      <p className="text-red-500">*</p>
+                                    )}
+                                  </div>
+                                  <div className="flex items-center gap-2">
+                                    <input
+                                      type="text"
+                                      className="w-full border border-gray-400 py-2 text-gray-900 outline-none placeholder:text-gray-400 focus:border-primary-300 rounded-lg"
+                                      id={value.id}
+                                    />
+                                    {isVisiblePDF && (
+                                      <button type="button" onClick={() => handleEdit(value.id)} className="text-blue-500">
+                                        <Pencil size={16} />
+                                      </button>
+                                    )}
+                                    {isVisiblePDF && !protectedFields.includes(value.id) && (
+                                      <button type="button" onClick={() => handleDelete(value.id)} className="text-red-500">
+                                        <Trash size={16} />
+                                      </button>
+                                    )}
+                                  </div>
                                 </div>
-                              </div>
-                            );
+                              );
+                            }
                           }}
                         />
+
                         <div className="mt-6">
                           <p className="text-gray-500 text-sm text-justify">
                             Al continuar, acepto las <span>políticas</span> aplicables para el tratamiento de mis datos personales según la jurisdicción local del responsable y de
